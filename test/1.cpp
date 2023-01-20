7372f8a2027fa58ccc258ecebab359fdf06ae05f
DreamLand7707 <1753913274@qq.com>
2023-01-20 10:31:57 +0800

    2023/1/20

--- test/1.cpp
+++ test/1.cpp
@@ -1,9 +1,70 @@
-﻿#include <map>
-#include <unordered_map>
+﻿#include <fstream>
+#include <iostream>
+#include <map>
 
+const short EXCESS = 4,
+            BYTE_SIZE = 8,
+            ALPHA = 256,
+            MASK1 = 255,
+            MASK2 = 15;
+bool bitsLeftOver = false;
+short leftOver;
+std::ofstream fout("./source/out.txt");
+std::ifstream fin("./source/in.txt");
+void output(int pcode)
+{
+    short c, d;
+    if (bitsLeftOver)
+    {
+        d = short(pcode & MASK1);
+        c = short((leftOver << EXCESS) | (pcode >> BYTE_SIZE));
+        fout.put(c);
+        fout.put(d);
+        bitsLeftOver = false;
+    }
+    else
+    {
+        leftOver = pcode & MASK2;
+        c = short(pcode >> EXCESS);
+        fout.put(c);
+        bitsLeftOver = true;
+    }
+}
+void compress()
+{
+    using namespace std;
+    map<int, short> h;
+    for (int i = 0; i < ALPHA; i++)
+    {
+        h.insert(pair(i, i));
+    }
+    short codesUsed = ALPHA;
 
+    short c = fin.get();
+    if (c != EOF)
+    {
+        int pcode = c;
+        while ((c = fin.get()) != EOF)
+        {
+            int theKey = (pcode << BYTE_SIZE) + c;
+            auto thepair = h.find(theKey);
+            if (thepair == h.end())
+            {
+                output(pcode);
+                h.insert(std::pair((pcode << BYTE_SIZE) | c, codesUsed++));
+                pcode = c;
+            }
+            else
+                pcode = thepair->second;
+        }
+        output(pcode);
+        if (bitsLeftOver)
+            fout.put(leftOver << EXCESS);
+    }
+    fout.close();
+    fin.close();
+}
 int main(void)
 {
-    std::unordered_map<int, double> e;
-    ;
+    compress();
 }
--- test/2.cpp
+++ test/2.cpp
@@ -1,1 +1,13 @@
-﻿
+﻿#include "basic_matr_tag.h"
+int main(void)
+{
+    using namespace std;
+    fstream fout("./source/in.txt", std::ios::out | std::ios::trunc);
+    default_random_engine engine;
+    uniform_int<char> unch(32, 126);
+    for (int i = 0; i < 100000000; i++)
+    {
+        fout.put(unch(engine));
+    }
+    return 0;
+}
